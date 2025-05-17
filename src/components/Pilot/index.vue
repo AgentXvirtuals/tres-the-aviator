857ae526836ac0e3e5f1c565cde5916fd5416c4c
AgentXvirtuals <ladillosicario@gmail.com>
2025-05-17 17:20:38 +0200

    init

--- src/components/Pilot/index.vue
+++ src/components/Pilot/index.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-import type { TresVector3 } from '@tresjs/core'
+import { TresVector3 } from '@tresjs/core'
 import PilotBody from './body.vue'
 import PilotFace from './face.vue'
 import PilotHairs from './hairs.vue'
 import PilotGlass from './glass.vue'
 import PilotEar from './ear.vue'
 
-defineProps<{ position?: TresVector3 }>()
+defineProps<{
+  position?: TresVector3
+}>()
 </script>
 
 <template>
-  <TresObject3D name="pilot" :position="position">
+  <TresObject3D name="penguin" :position="position">
     <PilotBody />
     <PilotFace />
-    <PilotHairs />
-    <PilotGlass />
-    <PilotEar />
+    <PilotGlass /> <!-- Pico -->
+    <PilotEar />   <!-- Alas -->
+    <!-- PilotHairs opcional -->
   </TresObject3D>
 </template>
